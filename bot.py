95795f6bc62b27d010a13b8a27b186c1f9281363
minecraftkaramel <minecraftkaramel@gmail.com>
2026-03-02 22:13:30 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -712,10 +712,10 @@ async def on_message(message):
                 arr = f.get("arr", {}).get("icao", "???") if isinstance(f.get("arr"), dict) else "???"
                 
                 # Формуємо красивий мінімалістичний рядок
-                desc_lines.append(f"{full_cs}` • {pilot} • {ac} • {dep} ➔ {arr}")
+                desc_lines.append(f"{full_cs} • {pilot} • {ac} • {dep} ➔ {arr}")
             
             # Створюємо фінальний Ембед
-            embed = discord.Embed(title="📡 Live Traffic - Ukraine Classic", description="\n".join(desc_lines), color=0x3498db)
+            embed = discord.Embed(title="📡 Live Traffic - Ukraine Classic Air Alliance", description="\n".join(desc_lines), color=0x3498db)
             
             # Додаємо час оновлення знизу
             current_utc_time = datetime.now(timezone.utc).strftime('%H:%M')
@@ -989,3 +989,4 @@ async def on_ready():
     client.loop.create_task(main_loop())
 
 client.run(DISCORD_TOKEN)
+
